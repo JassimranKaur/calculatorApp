620ae93e70193a440477a76b66b204ca19397a58
5im15jakaur <5im15jakaur@gibwintra.ch>
2017-12-06 09:41:56 +0100

    aenderung2

--- src/test/java/com/mycompany/calculatorapp/CalculatorApp/CalculatorTest.java
+++ src/test/java/com/mycompany/calculatorapp/CalculatorApp/CalculatorTest.java
@@ -31,7 +31,6 @@ public class CalculatorTest {
         double expResult = 8.0;
         Assert.assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
